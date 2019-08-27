15ffb684d1ca0bacc3a8be37fd8d5c6958449beb
Richie <richieganney@gmail.com>
2019-08-27 14:31:43 +0100

    withdraw method throws error if input is anything but an integer. method also adds date log and current balance to full_statement

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -9,7 +9,7 @@ class BankAccount
     @full_statement = []
   end
 
-  def calculate_account_balance
+  def account_balance
     @account_balance.sum
   end
 
@@ -17,13 +17,14 @@ class BankAccount
     raise 'Invalid input. Must input an integer' unless deposit_amount.is_a? Integer
 
     @account_balance.push(deposit_amount)
-    @full_statement.push(Time.now.to_s[0..9]).push(deposit_amount).push(calculate_account_balance)
+    @full_statement.push(Time.now.to_s[0..9]).push(deposit_amount).push(account_balance)
   end
 
   def withdraw(withdrawal_amount)
     raise 'Invalid input. Must input an integer' unless withdrawal_amount.is_a? Integer
     @account_balance.push(-withdrawal_amount)
+    @full_statement.push(Time.now.to_s[0..9]).push(-withdrawal_amount).push(account_balance)
   end
 end
 
-# binding.pry
+binding.pry
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -8,20 +8,20 @@ describe BankAccount do
   describe '#deposit' do
     it 'allows the user to desposit the first £50 into their bank account' do
       bank_account.deposit(50)
-      expect(bank_account.calculate_account_balance).to eq 50
+      expect(bank_account.account_balance).to eq 50
     end
 
-    it 'allows the user to desposit £30 into their bank account with a current balance of £45' do
+    it 'allows the user to desposit different amounts into their bank account' do
       bank_account.deposit(45)
       bank_account.deposit(30)
-      expect(bank_account.calculate_account_balance).to eq 75
+      expect(bank_account.account_balance).to eq 75
     end
 
     it 'should throw an error if an invalid input into the method' do
       expect { bank_account.deposit('65') }.to raise_error 'Invalid input. Must input an integer'
     end
 
-    it 'every deposit should include a date log and the current balance' do
+    it 'every deposit should include a date log and the current balance in the full statement' do
       bank_account.deposit(20)
       expect(bank_account.full_statement).to include(Time.now.to_s[0..9])
       expect(bank_account.full_statement).to include(20)
@@ -32,11 +32,25 @@ describe BankAccount do
     it 'allows a user to withdraw £50 from their bank account' do
       bank_account.deposit(50)
       bank_account.withdraw(50)
-      expect(bank_account.calculate_account_balance).to eq 0
+      expect(bank_account.account_balance).to eq 0
     end
 
     it 'should throw an error if an invalid input into the method' do
       expect { bank_account.withdraw('65') }.to raise_error 'Invalid input. Must input an integer'
+    end
+
+    it 'allows the user to withdraw different amounts from their bank account' do
+      bank_account.withdraw(10)
+      bank_account.withdraw(20)
+      expect(bank_account.account_balance).to eq -30
+    end
+
+    it 'every withdrawal should include a date log and the current balance in the full statement' do
+      bank_account.deposit(20)
+      bank_account.withdraw(10)
+      expect(bank_account.full_statement).to include(Time.now.to_s[0..9])
+      expect(bank_account.full_statement).to include(10)
+      expect(bank_account.full_statement).to include(-10)
     end
   end
 end
