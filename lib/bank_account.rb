1736fb6e1077a84f38df1175c92c967df012857f
Richie <richieganney@gmail.com>
2019-08-27 14:14:39 +0100

    program includes withdraw method that successfully withdraws the required amount

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,4 +1,5 @@
 # frozen_string_literal: true
+require 'pry'
 
 class BankAccount
   attr_reader :full_statement
@@ -16,6 +17,13 @@ class BankAccount
     raise 'Invalid input. Must input an integer' unless deposit_amount.is_a? Integer
 
     @account_balance.push(deposit_amount)
-    @full_statement.push(Time.now.to_s[0..9].to_s).push(deposit_amount).push(calculate_account_balance)
+    @full_statement.push(Time.now.to_s[0..9]).push(deposit_amount).push(calculate_account_balance)
+  end
+
+  def withdraw(withdrawal_amount)
+    raise 'Invalid input. Must input an integer' unless withdrawal_amount.is_a? Integer
+    @account_balance.push(-withdrawal_amount)
   end
 end
+
+# binding.pry
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -23,8 +23,20 @@ describe BankAccount do
 
     it 'every deposit should include a date log and the current balance' do
       bank_account.deposit(20)
-      expect(bank_account.full_statement).to include(Time.now.to_s[0..9].to_s)
+      expect(bank_account.full_statement).to include(Time.now.to_s[0..9])
       expect(bank_account.full_statement).to include(20)
+    end
+  end
+
+  describe '#withdraw' do
+    it 'allows a user to withdraw £50 from their bank account' do
+      bank_account.deposit(50)
+      bank_account.withdraw(50)
+      expect(bank_account.calculate_account_balance).to eq 0
+    end
+
+    it 'should throw an error if an invalid input into the method' do
+      expect { bank_account.withdraw('65') }.to raise_error 'Invalid input. Must input an integer'
     end
   end
 end
