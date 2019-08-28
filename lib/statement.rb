0615e6ffc8557ad9ae690ba9b39318c0964a8a00
Richie <richieganney@gmail.com>
2019-08-28 18:22:00 +0100

    shortened methods in statement class

--- lib/statement.rb
+++ lib/statement.rb
@@ -3,15 +3,19 @@
 require 'bank_account'
 
 class Statement
-  def self.request(statement)
+  def self.request_statement(statement)
     @statement = statement.reverse.each_slice(3).to_a.map do |statement_element|
       statement_element[1] < 0 ? statement_element[1] = "|| #{'%.2f' % -statement_element[1]}" : statement_element[1] = "#{'%.2f' % statement_element[1]} ||"
       statement_element[0] < 0 ? statement_element[0] = "|| #{'%.2f' % -statement_element[0]}" : statement_element[0] = "#{'%.2f' % statement_element[0]} ||"
       statement_element.reverse.join(' || ')
-    end.insert(0, 'date || credit || debit || balance')
+    end
   end
 
-  def self.print_statement
-    puts @statement
+  def self.create_table(input)
+    input.insert(0, 'date || credit || debit || balance')
+  end
+
+  def self.print_statement(statement)
+    puts Statement.create_table(Statement.request_statement(statement))
   end
 end
--- spec/view_statement_spec.rb
+++ spec/view_statement_spec.rb
@@ -6,29 +6,13 @@ require 'timecop'
 describe Statement do
   subject(:statement) { described_class.new }
 
-  describe '.request' do
-    let(:bank_account) { double :bank_account }
-
-    it 'renders the full bank statement, with one transaction, with a date, time and balance; ready to view' do
-      allow(bank_account).to receive(:full_statement) { [Time.now.to_s[0..9], 500, 500] }
-      expect(Statement.request(bank_account.full_statement)).to eq(['date || credit || debit || balance',
-                                                                    "#{Time.now.to_s[0..9]} || 500.00 || || 500.00 ||"])
-    end
-
-    it 'renders the full bank statement, with one multiple transactions, with a date, time and balance; ready to view' do
-      allow(bank_account).to receive(:full_statement) { [Time.now.to_s[0..9], 500, 500, Time.now.to_s[0..9], -50, 450] }
-      expect(Statement.request(bank_account.full_statement)).to eq(['date || credit || debit || balance', "#{Time.now.to_s[0..9]} || || 50.00 || 450.00 ||",
-                                                                    "#{Time.now.to_s[0..9]} || 500.00 || || 500.00 ||"])
-    end
-  end
-
   describe '.print_statement' do
     let(:bank_account) { double :bank_account }
 
     it 'outputs the bank statement to the console' do
       allow(bank_account).to receive(:full_statement) { [Time.now.to_s[0..9], 500, 500] }
-      Statement.request(bank_account.full_statement)
-      expect { Statement.print_statement }.to output.to_stdout
+      expect { Statement.print_statement(bank_account.full_statement) }.to output("date || credit || debit || balance\n
+                                                                                  2019-08-28 || 500.00 || || 500.00 ||\n").to_stdout
     end
   end
 end
