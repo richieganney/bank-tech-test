65688fa9be2c79b64488c97f4737632ddda62e97
Richie <richieganney@gmail.com>
2019-08-30 11:04:25 +0100

    time format now correct

--- README.md
+++ README.md
@@ -51,7 +51,7 @@ when you want to exit the program. This will run the tests for you.
 
 ## Example output
 
-date || credit || debit || balance  
-2019/08/27 || || 500.00 || 2500.00  
-13/01/2012 || 2000.00 || || 3000.00  
+date || credit || debit || balance
+14/01/2012 || || 500.00 || 2500.00
+13/01/2012 || 2000.00 || || 3000.00
 10/01/2012 || 1000.00 || || 1000.00
--- spec/view_statement_spec.rb
+++ spec/view_statement_spec.rb
@@ -4,15 +4,13 @@ require 'statement'
 require 'timecop'
 
 describe Statement do
-  subject(:statement) { described_class.new }
 
   describe '.print_statement' do
     let(:bank_account) { double :bank_account }
 
     it 'outputs the bank statement to the console' do
-      allow(bank_account).to receive(:full_statement) { [Time.now.to_s[0..9], 500, 500] }
-      expect { Statement.print_statement(bank_account.full_statement) }.to output("date || credit || debit || balance\n
-                                                                                  2019-08-28 || 500.00 || || 500.00 ||\n").to_stdout
+      allow(bank_account).to receive(:full_statement) { ["31/12/1999", 500, 500] }
+      expect { Statement.print_statement(bank_account.full_statement) }.to output("date || credit || debit || balance\n31/12/1999 || 500.00 || || 500.00 ||\n").to_stdout
     end
   end
 end
